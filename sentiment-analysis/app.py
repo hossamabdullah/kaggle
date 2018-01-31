d84c02dc4287e01dfa4d534ba42c309da6ddebd8
Hossam Abdullah <hossamabdalh@gmail.com>
2018-01-31 05:44:25 +0200

    adding the generation of submission file

--- sentiment-analysis/app.py
+++ sentiment-analysis/app.py
@@ -9,6 +9,7 @@ from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn import svm
 from sklearn.svm import LinearSVC
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+import csv
 
 def load_TrainingData(path):     #loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
     D = pd.read_csv(path, sep='\t', header=0)
@@ -28,7 +29,7 @@ X_train, Y_train, _ = load_TrainingData("./train.tsv")
 print X_train[:10]
 
 print "loading testing data"
-X_test, _ = load_TestingData("./test.tsv")
+X_test, X_phrase_id = load_TestingData("./test.tsv")
 
 print "splitting training data"
 x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=0.1, random_state=42)
@@ -40,6 +41,8 @@ print "applying vectorizer"
 vectorizer = TfidfVectorizer(stop_words='english')
 x_train_transformed = vectorizer.fit_transform(x_train)
 x_test_transformed  = vectorizer.transform(x_test)
+#actual Test (Capital X)
+actual_test_transformed = vectorizer.transform(X_test)
 
 print "applying svm algorithm"
 # clf = svm.SVC(decision_function_shape='ovo')
@@ -50,3 +53,11 @@ clf.fit(x_train_transformed, y_train)
 print "measuring the score"
 score = clf.score(x_test_transformed, y_test)
 print("the score is : ",score, "%")
+
+submission = open('scikit_submission.csv', 'w')
+csvwriter = csv.writer(submission, delimiter=',')
+csvwriter.writerow(['PhraseId', 'Sentiment'])
+
+predictions = clf.predict(actual_test_transformed)
+for i in range(len(predictions)):
+    csvwriter.writerow([str(X_phrase_id[i]), str(predictions[i])])
