1bd3b50e5b350bf01e1ee6340c609a9926cb0c65
Hossam Abdullah <hossamabdalh@gmail.com>
2018-01-31 05:35:18 +0200

    have a running instance

--- sentiment-analysis/app.py
+++ sentiment-analysis/app.py
@@ -7,6 +7,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn import svm
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 
 def load_TrainingData(path):     #loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
     D = pd.read_csv(path, sep='\t', header=0)
@@ -23,28 +25,36 @@ def load_TestingData(path):     #loads data , caluclate Mean & subtract it data,
 
 print "loading training data"
 X_train, Y_train, _ = load_TrainingData("./train.tsv")
+print X_train[:10]
+
 print "loading testing data"
 X_test, _ = load_TestingData("./test.tsv")
 
 print "splitting training data"
-x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=0.1, random_state=42)
+
+print x_train[:10]
+print x_train.shape
 
 print "applying vectorizer"
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+vectorizer = TfidfVectorizer(stop_words='english')
 x_train_transformed = vectorizer.fit_transform(x_train)
 x_test_transformed  = vectorizer.transform(x_test)
 
-### feature selection, because text is super high dimensional and
-### can be really computationally chewy as a result
+# x_train_transformed = np.asarray(x_train_transformed)
+# x_test_transformed = np.asarray(x_test_transformed)
+## feature selection, because text is super high dimensional and
+## can be really computationally chewy as a result
 print "applying feature selection"
-selector = SelectPercentile(f_classif, percentile=10)
+selector = SelectPercentile(percentile=10)
 selector.fit(x_train_transformed, y_train)
 x_train_transformed = selector.transform(x_train_transformed).toarray()
 x_test_transformed  = selector.transform(x_test_transformed).toarray()
 
+
 print "applying svm algorithm"
 # clf = svm.SVC(decision_function_shape='ovo')
-clf = GaussianNB()
+clf = OneVsOneClassifier(LinearSVC())
 # x_train = x_train.reshape(-1, 1)
 clf.fit(x_train_transformed, y_train)
 
